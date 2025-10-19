b6144a7e73e76f465265786b3c736c58ba447c37
omarcospo <marcos.felipe@usp.br>
2025-10-19 19:55:37 -0300

    remove gruvbox

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,26 +1,7 @@
 return {
 	{
-		"ellisonleao/gruvbox.nvim",
-		priority = 1000,
-		enabled = false,
-		config = function()
-			require("gruvbox").setup({
-				overrides = {
-					StatusLine = { bg = "#181617" },
-					NormalFloat = { bg = "NONE" },
-					FloatBorder = { bg = "NONE" },
-					WinSeparator = { bg = "NONE" },
-					SignColumn = { bg = "NONE" },
-				},
-				dim_inactive = false,
-			})
-			vim.cmd("colorscheme gruvbox")
-		end,
-	},
-	{
 		"catppuccin/nvim",
 		name = "catppuccin",
-		-- enabled = false,
 		priority = 1000,
 		config = function()
 			require("catppuccin").setup({

27976cac3195efd87193fbfb85a726447d245409
omarcospo <marcos.felipe@usp.br>
2025-10-19 19:55:59 -0300

    add csv editor

--- lua/plugins/mark.lua
+++ lua/plugins/mark.lua
@@ -14,4 +14,22 @@ return {
 		---@type render.md.UserConfig
 		opts = {},
 	},
+	{
+		"hat0uma/csvview.nvim",
+		---@module "csvview"
+		---@type CsvView.Options
+		opts = {
+			parser = { comments = { "#", "//" } },
+			keymaps = {
+				-- Text objects for selecting fields
+				textobject_field_inner = { "if", mode = { "o", "x" } },
+				textobject_field_outer = { "af", mode = { "o", "x" } },
+				jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
+				jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
+				jump_next_row = { "<Enter>", mode = { "n", "v" } },
+				jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
+			},
+		},
+		cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
+	},
 }

db54a68c6ccf46d3653162a57b4f1e0d0010068d
omarcospo <marcos.felipe@usp.br>
2025-10-19 19:55:29 -0300

    switch to yazi

--- lua/plugins/filemanager.lua
+++ lua/plugins/filemanager.lua
@@ -1,37 +1,32 @@
 return {
 	{
-		"is0n/fm-nvim",
-		version = false,
-		keys = {
-			{ "<Leader>ff", "<CMD>Lf<CR>" },
+		"mikavilpas/yazi.nvim",
+		version = "*", -- use the latest stable version
+		event = "VeryLazy",
+		dependencies = {
+			{ "nvim-lua/plenary.nvim", lazy = true },
 		},
-		config = function()
-			require("fm-nvim").setup({
-				on_close = {},
-				on_open = {
-					function()
-						vim.keymap.set("t", "<C-q>", "q", { silent = true, buffer = true })
-					end,
-				},
-				ui = {
-					default = "float",
-					float = {
-						border = "single",
-						height = 0.80,
-						width = 0.75,
-					},
-				},
-				mappings = {
-					vert_split = "<C-s>",
-					horz_split = "<C-h>",
-					tabedit = "<C-t>",
-					edit = "<C-e>",
-					ESC = "<ESC>",
-				},
-			})
+		keys = { { "<leader>f", "<cmd>Yazi cwd<cr>" } },
+		opts = {
+			open_for_directories = false,
+			floating_window_scaling_factor = 0.7,
+			yazi_floating_window_border = "single",
+			keymaps = {
+				open_file_in_vertical_split = "<c-v>",
+				open_file_in_horizontal_split = "<c-h>",
+				show_help = false,
+				copy_relative_path_to_selected_files = false,
+				grep_in_directory = false,
+				replace_in_directory = false,
+				cycle_open_buffers = false,
+				open_file_in_tab = false,
+				send_to_quickfix_list = false,
+				change_working_directory = false,
+				open_and_pick_window = false,
+			},
+		},
+		init = function()
+			vim.g.loaded_netrwPlugin = 1
 		end,
-	},
-	{
-		"VebbNix/lf-vim",
 	},
 }

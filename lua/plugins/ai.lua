f392532bf4d80bea325add00a17defac2fa939d7
omarcospo <marcos.felipe@usp.br>
2025-10-28 13:21:04 -0300

    better copilet view

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -10,37 +10,20 @@ return {
 	},
 
 	{
-		"yetone/avante.nvim",
-		event = "VeryLazy",
-		version = false,
-		opts = {
-			provider = "copilot",
-			windows = {
-				width = 40,
-				sidebar_header = { enabled = false },
-				ask = { floating = true, border = "single" },
+		"folke/sidekick.nvim",
+		cli = {
+			keys = {
+				hide_ctrl_q = { "<c-q>", "hide", mode = "nt", desc = "hide the terminal window" },
 			},
-			mappings = {
-				sidebar = {
-					-- apply_all = "A",
-					-- apply_cursor = "a",
-					-- retry_user_request = "r",
-					-- edit_user_request = "e",
-					-- switch_windows = "<Tab>",
-					-- reverse_switch_windows = "<S-Tab>",
-					-- remove_file = "d",
-					-- add_file = "@",
-					close = { "<Esc>", "q", "<C-q>", "<C-c>" },
-					close_from_input = { normal = "<Esc>", insert = "<C-q>" },
-				},
-			},
-			behaviour = { auto_approve_tool_permissions = { "replace_in_file" } },
-			input = { provider = "dressing" },
-			selector = { provider = "fzf" },
+			picker = "fzf-lua",
 		},
-		init = function()
-			vim.keymap.set("n", "<leader>aa", "<cmd>AvanteAsk<cr>")
-			vim.keymap.set("n", "<leader>ac", "<cmd>AvanteToggle<cr>")
-		end,
+		keys = {
+			{
+				"<leader>aa",
+				function()
+					require("sidekick.cli").toggle({ name = "copilot", focus = true })
+				end,
+			},
+		},
 	},
 }

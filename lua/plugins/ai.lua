14205016123eee2be43cdbf656ce79d585efd06c
omarcospo <marcos.felipe@usp.br>
2025-10-28 11:45:33 -0300

    better ai defaults

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,26 +1,46 @@
 return {
-	"yetone/avante.nvim",
-	build = vim.fn.has("win32") ~= 0 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
-		or "make",
-	event = "VeryLazy",
-	version = false,
-	---@module 'avante'
-	---@type avante.Config
-	opts = {
-		provider = "copilot",
-		windows = { ask = { floating = true, border = "single" } },
-		behaviour = {
-			auto_approve_tool_permissions = { "replace_in_file" },
-		},
-	},
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"MunifTanjim/nui.nvim",
+
+	{
 		"zbirenbaum/copilot.lua",
-		{
-			"MeanderingProgrammer/render-markdown.nvim",
-			opts = { file_types = { "markdown", "Avante" } },
-			ft = { "markdown", "Avante" },
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({})
+		end,
+	},
+
+	{
+		"yetone/avante.nvim",
+		event = "VeryLazy",
+		version = false,
+		opts = {
+			provider = "copilot",
+			windows = {
+				width = 40,
+				sidebar_header = { enabled = false },
+				ask = { floating = true, border = "single" },
+			},
+			mappings = {
+				sidebar = {
+					-- apply_all = "A",
+					-- apply_cursor = "a",
+					-- retry_user_request = "r",
+					-- edit_user_request = "e",
+					-- switch_windows = "<Tab>",
+					-- reverse_switch_windows = "<S-Tab>",
+					-- remove_file = "d",
+					-- add_file = "@",
+					close = { "<Esc>", "q", "<C-q>", "<C-c>" },
+					close_from_input = { normal = "<Esc>", insert = "<C-q>" },
+				},
+			},
+			behaviour = { auto_approve_tool_permissions = { "replace_in_file" } },
+			input = { provider = "dressing" },
+			selector = { provider = "fzf" },
 		},
+		init = function()
+			vim.keymap.set("n", "<leader>aa", "<cmd>AvanteAsk<cr>")
+			vim.keymap.set("n", "<leader>ac", "<cmd>AvanteToggle<cr>")
+		end,
 	},
 }

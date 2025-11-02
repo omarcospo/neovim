ef6d99feeb4f0e9ec1ace4f80e93d63673756e2d
omarcospo <marcos.felipe@usp.br>
2025-11-02 18:46:22 -0300

    remove git status float window

--- lua/plugins/sidebar.lua
+++ lua/plugins/sidebar.lua
@@ -53,32 +53,6 @@ return {
 						["<C-k>"] = "move_cursor_up",
 					},
 				},
-				git_status = {
-					window = {
-						position = "float",
-						mappings = {
-							["A"] = "git_add_all",
-							["gu"] = "git_unstage_file",
-							["gU"] = "git_undo_last_commit",
-							["ga"] = "git_add_file",
-							["gr"] = "git_revert_file",
-							["gc"] = "git_commit",
-							["gp"] = "git_push",
-							["gg"] = "git_commit_and_push",
-							["o"] = {
-								"show_help",
-								nowait = false,
-								config = { title = "Order by", prefix_key = "o" },
-							},
-							["oc"] = { "order_by_created", nowait = false },
-							["od"] = { "order_by_diagnostics", nowait = false },
-							["om"] = { "order_by_modified", nowait = false },
-							["on"] = { "order_by_name", nowait = false },
-							["os"] = { "order_by_size", nowait = false },
-							["ot"] = { "order_by_type", nowait = false },
-						},
-					},
-				},
 			})
 			vim.keymap.set("n", "<leader>e", ":Neotree<CR>", { desc = "Toggle file explorer" })
 			vim.api.nvim_create_autocmd({ "VimEnter" }, {

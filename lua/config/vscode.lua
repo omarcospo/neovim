2e48521c17a31cddb789856df62bb72b1c043da5
omarcospo <marcos.felipe@usp.br>
2025-11-20 19:06:43 -0300

    add more options for vscode

--- lua/config/vscode.lua
+++ lua/config/vscode.lua
@@ -1,3 +1,4 @@
+vim.o.cmdheight = 0
 --- Indentation
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
@@ -30,3 +31,18 @@ vim.keymap.set("n", "<C-j>", "<cmd>m .+1<cr>==")
 vim.keymap.set("n", "<C-k>", "<cmd>m .-2<cr>==")
 vim.keymap.set("v", "<C-j>", ":m '>+1<cr>gv=gv")
 vim.keymap.set("v", "<C-k>", ":m '<-2<cr>gv=gv")
+---- Comenting
+vim.keymap.set("n", "gco", "O<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>")
+vim.keymap.set("n", "gcO", "o<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>")
+---- Format and wrap
+vim.keymap.set("v", "w", ":'<,'>!fmt -80<CR>")
+---- Add thing at end of line
+function Add_at_end(char)
+	local cursor = vim.api.nvim_win_get_cursor(0)
+	vim.cmd("norm A" .. char)
+	vim.api.nvim_win_set_cursor(0, cursor)
+end
+vim.keymap.set("n", ";", ":lua Add_at_end(';')<CR>")
+vim.keymap.set("n", ",", ":lua Add_at_end(',')<CR>")
+vim.keymap.set("n", ".", ":lua Add_at_end('.')<CR>")
+vim.keymap.set({ "i", "v" }, "<C-q>", "<Esc>")
